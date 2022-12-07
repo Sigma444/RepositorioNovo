f45ef821bc0d6d97b5279dde3fde8528154db80f
moises-silva <mcastilho125@gmail.com>
2022-12-07 07:37:40 -0300

    Ciclo FOR

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Ciclo_FOR
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+
+           
+        {   //O comando define onde começa e quantos números diferentes podem ser somados 
+            int soma=0;
+            for (int i = 0; i < 3; i++)
+            {//O indivíduo deverá inserir um número
+                Console.WriteLine("Insira um número");
+             //Conversão da variável de string para inteiro
+                int num = int.Parse(Console.ReadLine());
+                soma += num;
+            }  //Defina a soma e o resultado final de todos os valores inseridos 
+            Console.WriteLine("A soma dos valores inseridos é: "+soma);
+
+            //FIM!
+            Console.ReadKey();  
+            
+        }
+    }
+}
