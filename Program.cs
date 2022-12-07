45a53c294ebecc9a437e68c6ca97155d9570cba2
moises-silva <mcastilho125@gmail.com>
2022-12-07 11:27:05 -0300

    debug

--- Program.cs
+++ Program.cs
@@ -12,20 +12,27 @@ namespace CICLO_WHILE
        
 
         static void Main(string[] args)
+        //Qualquer número inserido que seja diferente de zero será somado !
         { int num=1, soma=0; 
+        //A condição só valerá para números diferentes de zero!
             while (num!=0) 
             {
+        //O indivíduo irá inserir um valor para ser somaddo, e colocará "0" para finalizar o processo
                 Console.WriteLine("Insira um valor para somar! Insira 0 para parar");
+        //Conversão da variável String para inteiro, e ler a linha escrita !
                 num = int.Parse(Console.ReadLine());
-
+        // A soma resultará em um número
                 soma += num;
                 
             }
-
+        //Informa o indivíduo o valor da soma feita com os valores inseridos
             Console.WriteLine("A soma de todos os números inseridos é: " + soma);
             
+        //FIM! (Palmeiras nao tem mundial)
+            Console.ReadKey();  
 
 
-        }
+        } 
+        
     }
 }
