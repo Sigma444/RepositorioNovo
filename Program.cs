1940c4ed8f3ce8b9b13b5cd4a03140c172b3cbc6
moises-silva <mcastilho125@gmail.com>
2022-12-07 11:21:13 -0300

    aula 004

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CICLO_WHILE
+{
+    internal class Program
+    {
+       
+
+        static void Main(string[] args)
+        { int num=1, soma=0; 
+            while (num!=0) 
+            {
+                Console.WriteLine("Insira um valor para somar! Insira 0 para parar");
+                num = int.Parse(Console.ReadLine());
+
+                soma += num;
+                
+            }
+
+            Console.WriteLine("A soma de todos os números inseridos é: " + soma);
+            
+
+
+        }
+    }
+}
